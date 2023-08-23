20c6012ff6bf19ee59a72c84983ed72245324843
GeorgeOprea <oprea.george14@yahoo.com>
2023-08-23 17:08:37 +0300

    style:New styling

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const searchBtn = document.querySelector(".search-button");
 const displayWeather = document.querySelector(".display-div");
 const displayHumidity = document.querySelector(".humidity");
 const displayWind = document.querySelector(".wind");
+const displayCity = document.querySelector(".city-name");
+const displayTemp = document.querySelector(".temperature");
+const container = document.querySelector(".container");
+const displayInfo = document.querySelector(".display-div2");
 
 clearBtn.addEventListener("click", clearSearch);
 searchBtn.addEventListener("click", fetchWeather);
@@ -21,10 +25,11 @@ async function fetchWeather() {
 
     const currentData = await response.json();
     console.log(currentData);
-    displayWeather.textContent = currentData.main.temp + " " + "°C";
+    displayTemp.textContent = Math.round(currentData.main.temp) + " " + "°C";
     displayHumidity.textContent = "Humidity:" + " " + currentData.main.humidity;
     displayWind.textContent =
       "Wind:" + " " + currentData.wind.speed + " " + "mph";
+    displayCity.textContent = currentData.name;
   } catch (err) {
     console.log(
       "Something went wrong with fetching the current weather data...",
@@ -35,7 +40,8 @@ async function fetchWeather() {
 
 function clearSearch() {
   document.getElementById("search-city").value = "";
-  displayWeather.textContent = "";
+  displayTemp.textContent = "";
   displayHumidity.textContent = "";
   displayWind.textContent = "";
+  displayCity.textContent = "";
 }
