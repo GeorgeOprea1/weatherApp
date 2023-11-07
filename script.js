a0cec0fbe3aada7947fcd0b074e9d3ef61363f2c
GeorgeOprea <oprea.george14@yahoo.com>
2023-11-07 21:07:58 +0200

    Second version of the app, improved design

--- script.js
+++ script.js
@@ -7,17 +7,25 @@ const displayCity = document.querySelector(".city-name");
 const displayTemp = document.querySelector(".temperature");
 const container = document.querySelector(".container");
 const displayInfo = document.querySelector(".display-div2");
+const displayWeatherDescription = document.querySelector(
+  ".weather-description"
+);
+
+const displayDateTime = document.querySelector(".date-time");
 
 clearBtn.addEventListener("click", clearSearch);
-searchBtn.addEventListener("click", fetchWeather);
+searchBtn.addEventListener("click", getInputVal);
 
-async function fetchWeather() {
+function getInputVal() {
+  const searchCity = document.getElementById("search-city").value;
+  fetchWeather(searchCity);
+}
+
+async function fetchWeather(x) {
   try {
-    const searchCity = document.getElementById("search-city").value;
-
     const response = await fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
-        searchCity +
+        x +
         "&appid=3a6c02ef0bd9096b4844377e321485d2&units=metric",
 
       { mode: "cors" }
@@ -27,6 +35,19 @@ async function fetchWeather() {
     console.log(currentData);
     displayTemp.textContent = Math.round(currentData.main.temp) + " " + "°C";
     displayHumidity.textContent = "Humidity:" + " " + currentData.main.humidity;
+    displayWeatherDescription.textContent =
+      currentData.weather[0].description.charAt(0).toUpperCase() +
+      currentData.weather[0].description.slice(1);
+    displayDateTime.textContent =
+      new Date().toLocaleDateString("en-US", { day: "2-digit" }) +
+      " " +
+      new Date().toLocaleDateString("en-US", { month: "short" }) +
+      ", " +
+      new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
     displayWind.textContent =
       "Wind:" + " " + currentData.wind.speed + " " + "mph";
     displayCity.textContent = currentData.name;
@@ -44,4 +65,8 @@ function clearSearch() {
   displayHumidity.textContent = "";
   displayWind.textContent = "";
   displayCity.textContent = "";
+  displayDateTime.textContent = "";
+  displayWeatherDescription.textContent = "";
 }
+
+fetchWeather("Bucharest");
